ce65ca04536f8c89fcef2bff2d4e3af88b7a10f0
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-21 20:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+    let first = document.querySelector('input[name="fname"]');
+    let last = document.querySelector('input[name="lname"]');
 
+    let firstName = first.value;
+    let lastName = last.value;
+
+    if (firstName && lastName) {
+        alert(`First Name: ${firstName}\nLast Name: ${lastName}`)
+    } else {
+        alert('Please enter both First Name and Last Name.');
+    }
 }
